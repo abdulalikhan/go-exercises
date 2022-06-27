10f2a72aea4a679340d3c1d00e455de73741eec0
abdulalikhan <k190128@nu.edu.pk>
2022-06-27 10:09:00 +0500

    Solved problem: RNA Transcription

--- rna_transcription/rna_transcription.go
+++ rna_transcription/rna_transcription.go
@@ -1,3 +1,5 @@
+// Package hamming is a solution to the "RNA Transcription" problem
+// https://exercism.org/tracks/go/exercises/rna-transcription/
 package strand
 
 func ToRNA(dna string) string {
